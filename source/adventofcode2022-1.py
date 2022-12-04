c83a40b6bf4048f273e49cac1256021a54e897cb
oraclemaster <mauro.rospocher@gmail.com>
2022-12-04 16:28:30 +0100

    Update adventofcode2022-1.py

--- source/adventofcode2022-1.py
+++ source/adventofcode2022-1.py
@@ -3,21 +3,16 @@ import os
 #os.chdir('X:\\Sorgenti\\Python\\adventofcode\\2022')
 print ('Directory: ' + os.getcwd())
 
-numrec = 0
-max = 0
-somma = 0
+max = somma = 0
 
 in_file = open("input1.txt", "r")
 
 for line in in_file:
-    
-    numrec+=1
     if line.strip():   #se la riga non è formata da soli spazi
         somma = somma + int(line)
     else:
-        print("totale parziale: ", somma)
         if somma > max:
             max = somma
         somma = 0
 in_file.close()
-print("record letti ", numrec, " max: ", max)
+print("max: ", max)
