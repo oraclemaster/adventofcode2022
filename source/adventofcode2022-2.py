4d4d8c965b0d42bda97afe8d3019522d9da8254c
oraclemaster <mauro.rospocher@gmail.com>
2022-12-04 14:18:44 +0100

    Create adventofcode2022-2.py

---
+++ source/adventofcode2022-2.py
@@ -0,0 +1,23 @@
+import os
+import collections
+
+#os.chdir('X:\\Sorgenti\\Python\\adventofcode\\2022')
+print ('Directory: ' + os.getcwd())
+
+partite = open("input2.txt").read().strip().split("\n") # prod
+#partite = open("input2Test.txt").read().strip().split("\n") # Test
+#print ("partite ", partite)
+
+# A = Rock, B = Paper, C = Scissor first player, X = Rock, Y = Paper, Z = Scissor second player
+# score X = 1, Y = 2, Z = 3  + X = 0 loose, Y = 3 draw e Z = 6 if won
+# 0 "" nothing = 0
+# 1 BX = 1 for rock 0 lost                                                                        
+# 2 CY = 2 for paper 0 lost
+# 3 AZ = 3 for scissor 0 lost
+# 4 AX = 1 for rock + 3 for draw
+# 5 BY = 2 for paper + 3 for draw
+# 6 CZ = 3 for scissor + 3 for draw
+# 7 CX = 1 for rock + 6 for win
+# 8 AY = 2 for paper + 6 for win
+# 9 BZ = 3 for scissor + 6 for win
+sum(map(["", "B X","C Y","A Z", "A X", "B Y", "C Z", "C X", "A Y", "B Z"].index,partite))
