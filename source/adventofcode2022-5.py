325ff30fe1859c21ecd60890d8bc8783a51de69f
oraclemaster <mauro.rospocher@gmail.com>
2022-12-06 14:48:45 +0100

    Create adventofcode2022-5.py

---
+++ source/adventofcode2022-5.py
@@ -0,0 +1,26 @@
+from itertools import zip_longest
+
+rows = open('input5.txt').read().split('\n\n')
+
+# Create stack
+stack = rows[0].split('\n')
+del stack[-1]
+stack1 = transposed = reverselist = []
+for x in stack:
+    stack1.append([x[i:i + 4].translate({ord(i): None for i in ' []'}) for i in range(0, len(x), 4)])
+
+tranposed_tuples = zip_longest(*stack1, fillvalue=None)
+transposed_list = list(tranposed_tuples)
+transposed = [list(sublist) for sublist in transposed_list]
+transposed = [list(filter(None, lst)) for lst in transposed]
+
+command = [[int(y) for y in x.split() if y.isdigit()] for x in rows[1].split('\n') if x]
+command = [(a, b-1, c-1) for a,b,c in command]
+
+for a,b,c in command:
+    reverselist = transposed[b][:a]
+    reverselist = reverselist[::-1]
+    transposed[c] =  reverselist + transposed[c]
+    transposed[b] = transposed[b][a:]
+
+print("".join(x[0] for x in transposed))
