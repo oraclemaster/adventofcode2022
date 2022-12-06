1a22b14004adbdab29e7ccc3ab78f430a14d045e
oraclemaster <mauro.rospocher@gmail.com>
2022-12-06 14:39:41 +0100

    Update adventofcode2022-1b.py

--- source/adventofcode2022-1b.py
+++ source/adventofcode2022-1b.py
@@ -1,14 +1,11 @@
 import os
 
-#os.chdir('X:\\Sorgenti\\Python\\adventofcode\\2022')
-print ('Directory: ' + os.getcwd())
 in_file = open("input1.txt", "r")
 
 max = [0,0,0]
 totale = somma = 0
 
 for line in in_file:
-    
     numrec+=1
     if line.strip():   #se la riga non è formata da soli spazi
         somma = somma + int(line)
