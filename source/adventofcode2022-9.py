dfae79b3633be70bbb906bf511588dd9a0559e25
oraclemaster <mauro.rospocher@gmail.com>
2022-12-15 11:57:04 +0100

    Update adventofcode2022-9.py

--- source/adventofcode2022-9.py
+++ source/adventofcode2022-9.py
@@ -1,29 +1,45 @@
+from itertools import tee 
+
 position = ["0-0"]
 in_file = open("input9.txt", "r")
 
 ver = hor = vert = hort = 0
 for data in in_file:
     dir, dist = data.split()
-    for a in range(int(dist)):
-        if dir == "R":
-            hor +=1
-        elif dir == "L":
-            hor -= 1
-        elif dir == "U":
-            ver +=1
+    if dir == "R":
+        hor = hor + int(dist)
+    elif dir == "L":
+        hor = hor - int(dist)
+    elif dir == "U":
+        ver = ver + int(dist)
+    else:
+        ver == ver - int(dist)
+    print("posizione ", ver,hor)
+    if abs(ver - vert) > 1 or abs(hor-hort) > 1:
+        if hor > hort:
+            hort = hort + hor
         else:
-            ver -= 1
-        if abs(hor - hort) > 1 or abs(ver-vert) > 1:
-            if hor > hort:
-                hort +=1
-            elif hor < hort:
-                hort -=1
-            if ver > vert:
-                vert +=1
-            elif ver < vert:
-                vert -=1
-        posStr = str(hort)+"-"+ str(vert)
-        if posStr not in position:
-            position.append(posStr)
+            hort = hort - hor
+        if ver > vert:
+            vert = vert + ver
+        else:
+            vert = vert-ver
+    print("pos Tabil ", vert,hort)
+    posStr = str(vert)+"-"+ str(hort)
+    if posStr not in position:
+       position.append(posStr)
+    print (position)
 
+def follow(head):
+    x = y = 0 
+    for hx, hy in head:
+        if abs(hx - x) > 1 or abs(hy - y) > 1:
+            y += (hy > y) - (hy < y)
+            x += (hx > x) - (hx < x)
+        yield x, y
+
+second, tenth = tee(follow(move()))
+for _ in range(8):
+    tenth = follow(tenth)
 print("answer1 ", len(position))
+print("answer2 ", len(set(tenth)))
